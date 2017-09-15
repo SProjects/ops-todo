41ec7aa51d85ebec0ab0ace8d4c1d4cfc3dd2c55
Sebuuma Daniel <sedzsoft@gmail.com>
2017-09-15 16:54:45 +0300

    Adds Todo model, Todos blueprint and adds an endpoint to view all todos

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask_restplus import Api
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 from .config import configuration
 
-api = Api()
 db = SQLAlchemy()
 
 
@@ -13,7 +12,12 @@ def create_app(env_name):
     app.config.from_object(configuration[env_name])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.url_map.strict_slashes = False
-    api.init_app(app)
     db.init_app(app)
+
+    from api.v1.todos import todos as todos_blueprint
+    api = Api(todos_blueprint)
+    app.register_blueprint(todos_blueprint, url_prefix='/api/v1')
+    from api.v1.todos.views import Todos
+    api.add_resource(Todos, '/todos', endpoint='todos')
 
     return app
