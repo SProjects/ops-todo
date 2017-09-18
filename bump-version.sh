7268d8ab2c97858324b64dff2865899f858dd608
Sebuuma Daniel <sedzsoft@gmail.com>
2017-09-18 15:05:43 +0300

    Adds files to support automated versioning

---
+++ bump-version.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Get BUMP_LEVEL env variable to determine which figure to update
+NEW_VERSION=""
+
+# Read the VERSION file
+OLD_VERSION=`cat VERSION`
+
+OIFS="$IFS"
+IFS='.'
+read -a LEVELS <<< "${OLD_VERSION}"
+IFS="$OIFS"
+
+echo $OLD_VERSION
+
+MAJOR=${LEVELS[0]}
+MINOR=${LEVELS[1]}
+PATCH=${LEVELS[2]}
+
+# Update SEMVAR figure
+if [ "$BUMP_LEVEL" == "patch" ]; then
+	PATCH="$(($PATCH + 1))"
+elif [ "$BUMP_LEVEL" == "minor" ]; then
+    MINOR="$(($MINOR + 1))"
+else
+    MAJOR="$(($MAJOR + 1))"
+fi
+
+NEW_VERSION=${MAJOR}.${MINOR}.${PATCH}
+
+echo $NEW_VERSION
+
+echo $NEW_VERSION > VERSION
+
+TAG="v"${NEW_VERSION}
+
+# Add and commit the tag
+git add .
+git commit -m "Updates version: "${TAG}
+git tag -a "Tag "${TAG}
+
+# Push tag
+#git push --tags
